2c0f7321c33b16a1dce390f989ed6a6ce9f57575
hfp1355 <hfp1355@rit.edu>
2017-02-19 16:33:17 -0500

    Remove reference to GrassTile in entities

--- game/src/entities/index.js
+++ game/src/entities/index.js
@@ -1,9 +1,7 @@
 "use strict";
 
 var Player = require('./Player.js');
-var GrassTile = require('./GrassTile.js');
 
 module.exports = {
-  Player : Player,
-  GrassTile : GrassTile
+  Player : Player
 };
